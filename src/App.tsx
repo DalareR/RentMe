d1389750d4fffe417b508d437ce421268fd41560
Dalare Rana <dalarebuddy@gmail.com>
2023-04-19 13:11:49 -0400

    Created Hero Compon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Login from "./components/Login";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <NavBar />
+      <Hero />
       {/* <Login /> */}
     </div>
   );
---
+++ src/components/Hero.tsx
@@ -0,0 +1,52 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  HStack,
+  Heading,
+  Image,
+  Input,
+} from "@chakra-ui/react";
+import cover from "../assets/images/coupleInACar.jpg";
+import { useEffect, useState } from "react";
+
+function Hero() {
+  const [docHeight, setDocHeight] = useState(0);
+
+  useEffect(() => {
+    setDocHeight(window.innerHeight - 60);
+    window.addEventListener("resize", () =>
+      setDocHeight(window.innerHeight - 60)
+    );
+  }, []);
+
+  return (
+    <Box
+      display="flex"
+      justifyContent={{ base: "center", md: "start" }}
+      alignItems={{ base: "start", md: "center" }}
+      w="100vw"
+      h={`${docHeight}px`}
+      p={{ base: "10px", md: "25px" }}
+      bgImage={cover}
+      bgPos="center"
+      bgSize="cover"
+    >
+      <Box bg="white" minW="300px" maxW="500px" p="15px" borderRadius="5px">
+        <Heading fontSize="2xl" mb="10px">
+          Car Rentals
+        </Heading>
+        <FormControl>
+          <Input type="text" placeholder="Location" mb="5px" />
+          <HStack mb="10px">
+            <Input type="date" placeholder="From" />
+            <Input type="date" placeholder="" />
+          </HStack>
+          <Button colorScheme="brand">Search</Button>
+        </FormControl>
+      </Box>
+    </Box>
+  );
+}
+
+export default Hero;
