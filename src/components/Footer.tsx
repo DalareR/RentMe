f0b000729e724817f6eab4151f4bf131483c5e28
Dalare Rana <dalarebuddy@gmail.com>
2023-04-22 12:17:42 -0400

    Created Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,17 +13,18 @@ import {
   AiFillTwitterCircle,
   AiFillInstagram,
 } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <Box minH={200} p="20px">
+    <Box p="20px">
       <Heading size="lg" color="brand.500">
         Rent.me
       </Heading>
       <Divider />
       <Grid
         gridTemplateColumns={{ md: "1fr 1fr" }}
-        gridTemplateRows={{ base: `1fr 1fr` }}
+        gridTemplateRows={{ base: `1fr 1fr`, md: "auto" }}
         mt={5}
       >
         <GridItem>
@@ -41,9 +42,15 @@ function Footer() {
             align="center"
             gap={5}
           >
-            <Icon as={AiFillFacebook} boxSize={8} />
-            <Icon as={AiFillInstagram} boxSize={8} />
-            <Icon as={AiFillTwitterCircle} boxSize={8} />
+            <Link to="https://www.facebook.com">
+              <Icon as={AiFillFacebook} boxSize={8} cursor="pointer" />
+            </Link>
+            <Link to="https://www.instagram.com">
+              <Icon as={AiFillInstagram} boxSize={8} cursor="pointer" />
+            </Link>
+            <Link to="https://www.twitter.com">
+              <Icon as={AiFillTwitterCircle} boxSize={8} cursor="pointer" />
+            </Link>
           </Flex>
         </GridItem>
       </Grid>
