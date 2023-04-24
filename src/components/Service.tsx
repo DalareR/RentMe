6619f81c611855b1bceacbd6d2752371e0d6c607
Dalare Rana <dalarebuddy@gmail.com>
2023-04-24 12:08:49 -0400

    added animation to Service

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,5 +1,3 @@
-import yellowChevy from "../assets/images/yellowchevy.png";
-import carsVariety from "../assets/images/carsVariety.png";
 import {
   Flex,
   Stack,
@@ -11,35 +9,88 @@ import {
   ListItem,
 } from "@chakra-ui/react";
 import { services } from "../data/services";
+import { motion, spring } from "framer-motion";
+import { initial } from "lodash";
+import { useState } from "react";
+
+const variants = {
+  initial: {},
+  inView: {
+    transition: {
+      when: "afterChildren",
+    },
+  },
+};
+
+const variantsX = {
+  initial: {
+    x: -100,
+  },
+  inView: {
+    x: 0,
+    transition: {
+      duration: 1,
+      type: "spring",
+    },
+  },
+};
+
+const variantsY = {
+  initial: {
+    y: -100,
+  },
+  inView: {
+    y: 0,
+    transition: {
+      duration: 1,
+      type: "spring",
+    },
+  },
+};
 
 function Service() {
+  const [test, setTest] = useState(0);
   return (
     <Flex
       direction="column"
       align="center"
       p={{ lg: "50", md: "30", base: "10" }}
     >
-      <Heading color="brand.500" mb="5" textDecor={"underline"}>
-        Service
+      <Heading color="brand.500" mb="5" textDecor="underline">
+        Our Services
       </Heading>
       <Box>
         {services.map((service, index) => (
           <Flex
+            as={motion.div}
+            variants={variants}
+            initial="initial"
+            whileInView="inView"
             direction={{ base: "column", md: "row" }}
             align="center"
             overflow="hidden"
             key={index}
             className="serviceEven"
+            p={"25px"}
           >
-            <Stack className="serviceImage">
+            <Stack
+              as={motion.div}
+              variants={variantsX}
+              className="serviceImage"
+            >
               <Image
                 maxW={{ base: "300px", md: "400px", lg: "600px" }}
                 src={service.img}
                 flex="1"
               />
             </Stack>
-            <Stack mt={{ base: "5" }} ml={{ md: "5", lg: "5" }}>
-              <Heading>{service.title}</Heading>
+            <Stack
+              as={motion.div}
+              variants={variantsY}
+              mt={{ base: "5" }}
+              ml={{ md: "5", lg: "5" }}
+            >
+              <Heading color={"brand.500"}>{service.title}</Heading>
               <Text flex="1" size="lg">
                 {service.desc}
               </Text>
